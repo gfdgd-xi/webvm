24c7bb113a16147acb1c0677271ebb48fdc6594a
gfdgd_xi <3025613752@qq.com>
2024-12-07 16:40:52 +0800

    调整

--- config_github_gxde.js
+++ config_github_gxde.js
@@ -3,11 +3,11 @@ export const diskImageUrl = IMAGE_URL;
 // The root filesystem backend type
 export const diskImageType = "github";
 // Print an introduction message about the technology
-export const printIntro = true;
+export const printIntro = false;
 // Is a graphical display needed
 export const needsDisplay = true;
 // Executable full path (Required)
-export const cmd = "/sbin/init"; // Default: "/bin/bash";
+export const cmd = "xinit"; // Default: "/bin/bash";
 // Arguments, as an array (Required)
 export const args = []; // Default: ["--login"];
 // Optional extra parameters
